4eb974750d6dd58cafdaaed74ddfd12e82ab796e
Abhishek Dubey <7abhishek1410@gmail.com>
2023-04-23 00:37:08 +0530

    rotation-added

--- src/components/canvas/CameraRig.jsx
+++ src/components/canvas/CameraRig.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React,{useRef} from 'react'
+import {easing} from 'maath'
+import { useSnapshot } from 'valtio'
+import state from '../../store'
+import { useFrame } from '@react-three/fiber'
 
-const CameraRig = () => {
+
+const CameraRig = ({children}) => {
+  const snap = useSnapshot(state)
+  const group = useRef();
+
+  useFrame((state, delta)=>{
+
+    const isBreakPont = window.innerWidth<=1260
+    const isMobile = window.innerWidth<=600
+
+    // initial position of the mode
+
+    let targetPosition = [-0.4, 0, 2];
+
+    if(snap.intro)
+    {
+      if(isBreakPont)
+      {
+        targetPosition = [-0.4, 0, 2];
+      }else if(isMobile)
+      {
+        targetPosition = [0, 0.2, 2.5];
+
+      }
+    }else{
+      if(isMobile){
+        targetPosition = [0, 0, 2.5]
+      }
+      else {
+        targetPosition = [0, 0, 2];
+      }
+    }
+
+
+    // model camera postion
+
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta)
+
+
+    // easing for smooth rotation
+  
+  easing.dampE(
+    group.current.rotation,
+    [state.pointer.y / 10, -state.pointer.x / 5, 0],
+    0.25,
+    delta
+  )
+  })
+
+  
+
   return (
-    <div>CameraRig</div>
+
+    <group ref={group}>
+      {children}
+    </group>
   )
 }
 
